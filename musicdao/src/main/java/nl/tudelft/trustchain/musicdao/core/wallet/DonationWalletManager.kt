6d727ba8b06df4c51ae035da756556bcbec49dcb
NMRadu <R.Nicolae@student.tudelft.nl>
2025-05-29 18:27:32 +0200

    Lottery spends half the money

--- musicdao/src/main/java/nl/tudelft/trustchain/musicdao/core/wallet/DonationWalletManager.kt
+++ musicdao/src/main/java/nl/tudelft/trustchain/musicdao/core/wallet/DonationWalletManager.kt
@@ -1,24 +1,27 @@
 package nl.tudelft.trustchain.musicdao.core.wallet
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import android.content.Context
 import android.util.Log
 import dagger.hilt.android.qualifiers.ApplicationContext
-import javax.inject.Inject
-import java.util.concurrent.atomic.*
-import org.bitcoinj.core.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import nl.tudelft.trustchain.musicdao.core.repositories.ArtistRepository
+import org.bitcoinj.core.Coin
+import org.bitcoinj.core.ECKey
+import org.bitcoinj.core.PeerAddress
 import org.bitcoinj.core.listeners.DownloadProgressTracker
 import org.bitcoinj.kits.WalletAppKit
 import org.bitcoinj.params.RegTestParams
-import org.bitcoinj.wallet.*
 import java.net.InetAddress
 import java.net.UnknownHostException
-import java.util.*
-import java.util.concurrent.*
-import kotlinx.coroutines.*
-import nl.tudelft.trustchain.musicdao.core.repositories.ArtistRepository
-import nl.tudelft.trustchain.musicdao.core.repositories.model.Artist
+import java.util.Date
+import javax.inject.Inject
+
 
 const val REG_TEST_FAUCET_IP = "131.180.27.224"
 
@@ -117,7 +120,7 @@ class DonationWalletManager
 
         fun startLottery() {
             if (!::walletKit.isInitialized) {
-                Log.e("DonationWallet", "Cannot start lottery: Wallet not initialized")
+                Log.e("DonationWalletLottery", "Cannot start lottery: Wallet not initialized")
                 return
             }
 
@@ -128,7 +131,7 @@ class DonationWalletManager
                     try {
                         // Request money from faucet
                         if (!walletKit.isRunning || walletKit.wallet() == null) {
-                            Log.d("DonationWallet", "Waiting for wallet to be ready...")
+                            Log.d("DonationWalletLottery", "Waiting for wallet to be ready...")
                             delay(10000)
                             continue
                         }
@@ -136,67 +139,77 @@ class DonationWalletManager
                         // Get current balance
                         val balance = walletService.confirmedBalance()
                         if (balance == null ) {
-                            Log.i("DonationWallet", "The balance is null for distribution")
+                            Log.i("DonationWalletLottery", "The balance is null for distribution")
                             delay(10000) // Wait 10 seconds before next attempt
                             continue
                         }
 
-                        if(balance.isZero) {
-                            Log.i("DonationWallet", "Balance is zero requesting money from faucet")
-                            val faucetResult = walletService.defaultFaucetRequest()
-
-                            if (faucetResult) {
-                                Log.i("DonationWallet", "Successfully requested money from faucet")
-                            } else {
-                                Log.e("DonationWallet", "Failed to request money from faucet")
-                            }
-                            delay(1000)
-                            continue
-                        }
-
-                        Log.e("DonationWallet", "Current balance is ${balance.toFriendlyString()}")
+                        Log.e("DonationWalletLottery", "Current balance is ${balance.toFriendlyString()}")
 
                         // Get all artists
                         val artists = artistRepository.getArtists()
                         if (artists.isEmpty()) {
-                            Log.i("DonationWallet", "No artists found to distribute donations")
+                            Log.i("DonationWalletLottery", "No artists found to distribute donations")
                             delay(10000) // Wait 10 seconds before next attempt
                             continue
                         }
 
                         // Calculate amount per artist (1/n of total balance)
-                        val amountPerArtist = balance.divide(artists.size.toLong())
-                        Log.i("DonationWallet", "Distributing ${amountPerArtist.toFriendlyString()} to each artist")
+                        val amountPerArtist = balance.divide(artists.size.toLong()).divide(2)
+                        Log.i("DonationWalletLottery", "Distributing ${amountPerArtist.toFriendlyString()} to each artist")
 
-//                         Send to each artist
-                         artists.forEach { artist ->
-                             try {
-                                 val result = walletService.sendCoins(artist.bitcoinAddress, amountPerArtist.toPlainString())
-                                 if (result) {
-                                     Log.i("TestArtist", "Successfully sent ${amountPerArtist.toFriendlyString()} to ${artist.name} (${artist.bitcoinAddress})")
-                                 } else {
-                                     Log.e("TestArtist", "Failed to send coins to ${artist.name} (${artist.bitcoinAddress})")
-                                 }
-                             } catch (e: Exception) {
-                                 Log.e("DonationWallet", "Error sending coins to ${artist.name}: ${e.message}")
-                             }
-                         }
+                        val threshold = Coin.valueOf(5000)
 
-//                        val artist3173 = artists.find { it.name == "Artist 3173" }
-//                        if (artist3173 != null) {
-//                            Log.i("CheckArtist", "Found Artist 3173 with address: ${artist3173.bitcoinAddress}")
-//                            val result = walletService.sendCoins(artist3173.bitcoinAddress, amountPerArtist.toPlainString())
-//                            if (result) {
-//                                Log.i("DonationWallet", "Successfully sent ${amountPerArtist.toFriendlyString()} to ${artist3173.name} (${artist3173.bitcoinAddress})")
-//                            } else {
-//                                Log.e("DonationWallet", "Failed to send coins to ${artist3173.name} (${artist3173.bitcoinAddress})")
-//                            }
-//                        } else {
-//                            Log.w("CheckArtist", "Artist 3173 not found in artist list")
-//                        }
+                        Log.i("TestArtist", "TestTag")
+
+                        if(amountPerArtist.isLessThan(threshold)) {
+                            Log.i("DonationWalletLottery", "The amount per artist is less than 0.00005, not enough balance for distribution")
+                            delay(10000) // Wait 10 seconds before next attempt
+                            continue
+                        }
+
+
+                        //Send to each artist
+                        val recipients = mutableListOf<Pair<String, String>>()
+                        artists.forEach { artist -> recipients.add(Pair(artist.bitcoinAddress, amountPerArtist.toPlainString())) }
+                        try {
+                            val result = walletService.sendBatchTransaction(recipients)
+                            if (result) {
+                                Log.i("TestArtist", "Successfully sent batch transaction)")
+                            } else {
+                                Log.e("TestArtist", "Failed to send batch transaction")
+                            }
+                        } catch (e: Exception) {
+                            Log.e("DonationWalletLottery", "Error sending batch transaction")
+                        }
+
+//                       val artist3173 = artists.find { it.name == "Artist 3173" }
+//                       if (artist3173 != null) {
+//                            val recipients = mutableListOf<Pair<String, String>>()
+//                            recipients.add(Pair(artist3173.bitcoinAddress, amountPerArtist.toPlainString()))
+//
+//                           Log.i("TestArtist", "Found Artist 3173")
+//                           val result = walletService.sendBatchTransaction(recipients)
+//                           if (result) {
+//                               Log.i("TestArtist", "Successfully sent ${amountPerArtist.toFriendlyString()} to ${artist3173.name} (${artist3173.bitcoinAddress})")
+//                               Log.i("TestArtist", "Fee: ${walletService.estimateFee(recipients)}")
+//                               delay(1000)
+//                               val balanceTest = walletService.confirmedBalance()
+//                               if (balanceTest == null ) {
+//                                   Log.e("TestArtist", "Should not be possible")
+//                               }
+//                               else{
+//                                   Log.d("TestArtist", "Current balance is ${balanceTest.toFriendlyString()}")
+//                               }
+//                           } else {
+//                               Log.e("TestArtist", "Failed to send coins to ${artist3173.name} (${artist3173.bitcoinAddress})")
+//                           }
+//                       } else {
+//                           Log.w("TestArtist", "Artist 3173 not found in artist list")
+//                       }
 
                     } catch (e: Exception) {
-                        Log.e("DonationWallet", "Error in lottery distribution: ${e.message}")
+                        Log.e("DonationWalletLottery", "Error in lottery distribution: ${e.message}")
                     }
 
                     delay(10000) // Wait 10 seconds before next distribution
@@ -217,3 +230,4 @@ class DonationWalletManager
             }
         }
     }
+
